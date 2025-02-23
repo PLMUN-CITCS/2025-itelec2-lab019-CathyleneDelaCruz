1d5f17d6352995532ae5ff413362220222c52c69
BSIT Dela Cruz <cathydelacruz010@gmail.com>
2025-02-24 00:22:20 +0800

    Submitting Python Week 05 - Laboratory # 19

---
+++ grade_calculator_functions.py
@@ -0,0 +1,13 @@
+user_input = input("Enter your score: ")
+grade = int(user_input)
+if grade >= 90:
+    letter_grade = "A"
+elif grade >= 80:
+    letter_grade = "B"
+elif grade >= 70:
+    letter_grade = "C"
+elif grade >= 60:
+    letter_grade = "D"
+else:
+    letter_grade = "F"
+print("Your grade is:", letter_grade)
